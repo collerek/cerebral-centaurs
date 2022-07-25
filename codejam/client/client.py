3002bdee8439ea693f2b0f8973d2e09492671fef
Turtle <hypoturtle+gh@gmail.com>
2022-07-25 09:08:00 +0100

    added tools and drawable canvas adjustor

--- codejam/client/client.py
+++ codejam/client/client.py
@@ -7,7 +7,7 @@ import websockets
 from kivy.app import async_runTouchApp
 from kivy.graphics import Color, Line
 from kivy.lang.builder import Builder
-from kivy.properties import ListProperty, ObjectProperty, StringProperty
+from kivy.properties import BoundedNumericProperty, ListProperty, ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 
@@ -21,22 +21,27 @@ class TestCanvas(Widget):
     """TestCanvas Widget"""
 
     colour = ListProperty([random(), 1, 1])
+    line_width = BoundedNumericProperty(2, min=1, max=50, errorvalue=1)
 
     def on_touch_down(self, touch):
         """Called when a touch down event occurs"""
-        if self.collide_point(*touch.pos):
+        if self.collide_point(touch.x - self.offset_x, touch.y - self.offset_y):
             with self.canvas:
                 Color(*self.colour, mode="hsv")
-                touch.ud["line"] = Line(points=(touch.x, touch.y), width=2)
+                touch.ud["line"] = Line(points=(touch.x, touch.y), width=self.line_width)
 
     def on_touch_move(self, touch):
         """Called when a touch move event occurs"""
-        if self.collide_point(*touch.pos):
+        if self.collide_point(touch.x - self.offset_x, touch.y - self.offset_y):
             if touch.ud.get("line"):
                 with self.canvas:
                     touch.ud["line"].points += (touch.x, touch.y)
                 self.parent.message = json.dumps(
-                    {"line": touch.ud["line"].points[-4:], "colour": self.colour}
+                    {
+                        "line": touch.ud["line"].points[-4:],
+                        "colour": self.colour,
+                        "width": self.line_width,
+                    }
                 )
 
 
@@ -59,7 +64,7 @@ class WhiteBoard(BoxLayout):
         if parsed["client_id"] != client_id:
             with self.canvas:
                 Color(hsv=parsed["data"]["colour"])
-                Line(points=parsed["data"]["line"], width=2)
+                Line(points=parsed["data"]["line"], width=parsed["data"]["width"])
 
 
 root = Builder.load_file(f"{full_path}")
@@ -77,9 +82,7 @@ async def run_websocket(root):
                         print("sending " + m)
                         await websocket.send(m)
                     try:
-                        root.received = await asyncio.wait_for(
-                            websocket.recv(), timeout=1 / 60
-                        )
+                        root.received = await asyncio.wait_for(websocket.recv(), timeout=1 / 60)
                     except asyncio.exceptions.TimeoutError:
                         continue
                     await asyncio.sleep(1 / 60)
