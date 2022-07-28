3acae9f26d329ece6b6b271c1f26b7cae2ae69a3
Palmirka <kamila.goszcz.kg@wp.pl>
2022-07-28 17:59:11 +0200

    Add lobby

--- codejam/client/client.py
+++ codejam/client/client.py
@@ -16,6 +16,7 @@ from kivy.properties import BoundedNumericProperty, ListProperty, ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.modalview import ModalView
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.widget import Widget
 
@@ -220,6 +221,7 @@ class WhiteBoard(BoxLayout):
         self.game_callbacks: Dict[str, Callable[[Message], None]] = {
             GameOperations.CREATE.value: self.game,
             GameOperations.JOIN.value: self.game,
+            GameOperations.START.value: self.game,
         }
         self.chat_callbacks: Dict[str, Callable[[Message], None]] = {}
         self.error_callbacks: Dict[str, Callable[[Message], None]] = {
@@ -318,6 +320,36 @@ class WhiteBoardScreen(Screen):
                     print("Loop finished")
         except (ConnectionRefusedError, asyncio.exceptions.TimeoutError) as e:
             print("Connection refused", e)
+
+
+class LobbyButton(Button):
+    """Button for lobby"""
+
+    def __init__(self, **kwargs):
+        """Init button"""
+        super().__init__(**kwargs)
+        self.text = "Start game"
+        self.pos_hint = {"x": 0.3, "y": 0.4}
+        self.size_hint = [0.4, 0.2]
+
+    def on_release(self):
+        """Start game"""
+        root_widget.current_screen.wb.message = Message(
+            topic=Topic(type=TopicEnum.GAME, operation=GameOperations.START),
+            username=root_widget.current_screen.manager.username,
+            game_id=root_widget.current_screen.manager.game_id,
+        ).json(models_as_dict=True)
+        self.disabled = True
+        self.opacity = 0
+
+
+class Lobby(RelativeLayout):
+    """Lobby"""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        lobby = LobbyButton()
+        self.add_widget(lobby)
 
 
 class ErrorPopup(ModalView):
