7fa673bf9dfa898433c712129cb2141b6039aa0f
collerek <collerek@gmail.com>
2022-07-30 17:29:11 +0200

    remove unnecessary mock

--- tests/unit/test_server/test_running_game.py
+++ tests/unit/test_server/test_running_game.py
@@ -368,14 +368,7 @@ def test_leaving_game_by_creator(
     game_start_message: Message,
     game_leave_message: Message
 ):
-    async def dummy_delay_wrapper(delay: int, coro: Coroutine):
-        await asyncio.sleep(1)
-        await coro
 
-    mocker.patch(
-        "codejam.server.controllers.game_controller.delay_wrapper",
-        dummy_delay_wrapper
-    )
     mocker.patch(
         "codejam.server.controllers.chat_controller.ChatController.wait_till_next_turn",
         mocker.AsyncMock()
