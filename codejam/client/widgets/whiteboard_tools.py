2eda4d0dea599baedb1cb79c8be929708bf71024
collerek <collerek@gmail.com>
2022-07-31 19:44:08 +0200

    fix text filtering

--- codejam/client/widgets/whiteboard_tools.py
+++ codejam/client/widgets/whiteboard_tools.py
@@ -1,19 +1,15 @@
+import re
+
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.modalview import ModalView
+from kivy.uix.textinput import TextInput
 
 
 class Instructions(BoxLayout):
     """Instructions rule"""
 
     _canvas = ObjectProperty(None)
-
-
-class CanvasTools(GridLayout):
-    """CanvasTools rule"""
-
-    ...
 
 
 class InfoPopup(ModalView):
@@ -23,3 +19,12 @@ class InfoPopup(ModalView):
     title = StringProperty("")
     message = StringProperty("")
     additional_message = StringProperty("")
+
+
+class FilteredTextInput(TextInput):
+    """Game Buttons on menu screen"""
+
+    def insert_text(self, substring: str, from_undo: bool = False):
+        """Filter text to allow only alpha numeric w/o spaces"""
+        s = re.sub(r"[^A-Za-z0-9]+", "", substring)
+        return super().insert_text(s, from_undo=from_undo)
